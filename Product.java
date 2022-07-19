3e50c24d5c21ff2e9b891ad975b257dffa2b57a6
paitech <shreepaitech@gmail.com>
2022-07-19 02:36:20 +0000

    2nd line added

--- Product.java
+++ Product.java
@@ -3,5 +3,6 @@ class Product
     public static void main(String args[]){  
      System.out.println("Product Page");  
      System.out.println("New1");  
+     System.out.println("New2);  
     }  
 } 
